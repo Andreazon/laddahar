bcb3c2dbb00150de17f6a8d1252fd4d32000f777
Andreazon <139340528+Andreazon@users.noreply.github.com>
2026-01-29 10:57:19 +0100

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -3,8 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
+console.log("Appen startar...");
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
+  console.error("Kunde inte hitta root-elementet!");
   throw new Error("Could not find root element to mount to");
 }
 
